a4ea1c64cd494eb71aca3705ecb6c28cf592d032
adm400ba <querojogaminecraft184@gmail.com>
2026-08-05 17:18:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,36 @@ import edge_tts
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
+from langdetect import detect
 
 app = FastAPI()
 
 class Texto(BaseModel):
     texto: str
 
+vozes = {
+    "pt": "pt-BR-AntonioNeural",
+    "en": "en-US-GuyNeural",
+    "es": "es-ES-AlvaroNeural",
+    "fr": "fr-FR-HenriNeural",
+    "de": "de-DE-ConradNeural",
+    "it": "it-IT-DiegoNeural",
+    "ja": "ja-JP-KeitaNeural",
+    "ko": "ko-KR-InJoonNeural",
+    "zh-cn": "zh-CN-YunxiNeural"
+}
+
 @app.post("/gerar-audio")
 async def gerar_audio(data: Texto):
+    idioma = detect(data.texto)
+
+    voz = vozes.get(idioma, "en-US-GuyNeural")
+
     arquivo = "voz.mp3"
 
     communicate = edge_tts.Communicate(
         data.texto,
-        "pt-BR-AntonioNeural"
+        voz
     )
 
     await communicate.save(arquivo)
